d3f351aabafc8dc48c1b36844b4486fc2a79e358
dustin1709 <duong.chu.vu@lalumiere.org>
2022-06-21 15:57:41 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
+import React from "react";
 import './App.css';
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <div className="App">
       <main className="App">
-
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
       </main>
     </div>
   );
---
+++ src/pages/login/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Login = () => {
+    
+    return (
+        <>
+        <div>
+            <h1 style={{color: '#B6020F', fontSize: '120%'}}>Karo.land</h1>
+        </div>
+        </>
+    );
+
+}
+
+export default Login
