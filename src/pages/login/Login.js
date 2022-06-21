dd3ff552e40237f641a6634f81831cf469d6037d
dustin1709 <duong.chu.vu@lalumiere.org>
2022-06-21 16:57:12 +0700

    later

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -15,6 +15,12 @@ const Login = () => {
 
                 <button id="login-button" type="submit">Đăng nhập</button>
                 
+                <div style={{width: '100%', height: '9px', borderBottom: '1px solid #808080', textAlign: 'center', marginTop: '3%'}}>
+                    <span style={{fontSize: '15px', padding: '0 10px', color : '#808080', background: '#ffff'}}>
+                        Hoặc
+                    </span>
+                </div>
+
             </form>
         </div>
         </>
