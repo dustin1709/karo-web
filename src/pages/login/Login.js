a9ec2471a230d164417499625fa5016b3212b84d
dustin1709 <duong.chu.vu@lalumiere.org>
2022-06-21 16:37:42 +0700

    commit

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,11 +1,21 @@
 import React from "react";
+import './Login.css';
 
 const Login = () => {
     
     return (
         <>
         <div>
-            <h1 style={{color: '#B6020F', fontSize: '120%'}}>Karo.land</h1>
+            <form id="login-form">
+                <h1 id="h1-login">Karo.land</h1>
+                <input type="text" id="uname" name="uname" 
+                    placeholder="Tài khoản" required />
+                <input type="text" id="password" name="password"
+                    placeholder="Mật khẩu" required />
+
+                <button id="login-button" type="submit">Đăng nhập</button>
+                
+            </form>
         </div>
         </>
     );
