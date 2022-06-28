3d12fa96e19e572b0c31b992352e7cba4701c00b
dustin1709 <duong.chu.vu@lalumiere.org>
2022-06-28 16:00:17 +0700

    edit Register.js

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,38 +1,30 @@
-import { React, useState, useEffect } from "react";
+import { React, useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import Footer from "../../components/Footer";
+import Cities from "./hooks/Cities"; // --> import custom hook
 
 const Register = () => {
-
-    const cities = [
-        {
-            id: 1,
-            name:"Hà Nội",
-            dist: [
-                {id: 1, name: "Hoàn Kiếm"},
-                {id: 2, name: "Ba Đình"},
-                {id: 3, name: "Đống Đa"},
-                {id: 4, name: "Hai Bà Trưng"},
-                {id: 5, name: "Thanh Xuân"},
-                {id: 6, name: "Tây Hồ"},
-                {id: 7, name: "Cầu Giấy"},
-                {id: 8, name: "Hoàng Mai"},
-                {id: 9, name: "Long Biên"},
-                {id: 10, name: "Đông Anh"},
-                {id: 11, name: "Gia Lâm"}
-            ]
-        }, "TP Hồ Chí Minh", "Bình Dương", "Đà Nẵng", "Hải Phòng",
-                    "Long An", "Bà Rịa Vũng Tàu", "An Giang", "Bắc Giang", "Bắc Kạn",
-                    "Bạc Liêu", "Bắc Ninh", "Bến Tre", "Bình Định", "Bình Phước",
-                    "Bình Thuận", "Cà Mau", "Cần Thơ", "Cao Bằng", "Đắk Lắk", "Đắk Nông",
-                    "Điện Biên", "Đồng Nai", "Đồng Tháp", "Gia Lai", "Hà Giang", "Hà Nam",
-                    "Hà Tĩnh", "Hải Dương", "Hậu Giang", "Hoà Bình", "Hưng Yên", "Khánh Hoà",
-                    "Kiên Giang", "Kon Tum", "Lai Châu", "Lâm Đồng", "Lạng Sơn", "Lào Cai"
-                ];
+    
+    // select cities which allows districts' dropdown list to update
+    const cities = Cities();
+    const selectedCity = useRef(1);
+    const [listDistrict, setListDistrict] = useState([]);    
+    const changeSelectOptionHandler = (e) => {
+        selectedCity.current = e.target.value;
+        let districts = cities[selectedCity.current-1].dist;
+        setListDistrict(districts);
+        console.log("User choose city: " + selectedCity.current + ". The districts are: " + districts);
+    };
+    useEffect(() => {
+        if (selectedCity.current === 1) {
+            let districts = cities[0].dist;
+            setListDistrict(districts);
+        }
+    }, []);
 
     return (
         <>
-            <div id="background" style={{paddingTop: '3em'}}>
+            <div id="background" style={{ paddingTop: '3em' }}>
                 <form id="login-form">
                     <div id="title">
                         <img src={'/logo_karo.png'} />
@@ -47,16 +39,18 @@ const Register = () => {
 
                     <label style={{float: 'left', paddingTop: '2%'}}>Khu vực hoạt động</label>
                     <div style={{clear: 'both'}}></div>
-                    <select style={{float: 'left', width: '50%'}}  className="form-select" aria-label=".form-select-lg example">
+                    <select style={{float: 'left', width: '50%'}} onChange={changeSelectOptionHandler}  className="form-select" aria-label=".form-select-lg example">
                         {
-                            cities.map((city, index) => (
-                                <option value={index+1}>{city}</option>
+                            cities.map(city => (
+                                <option value={city.id}>{city.name}</option>
                             ))
                         }
                     </select>
 
                     <select style={{float: 'right', width: '48%'}} className="form-select" aria-label=".form-select-lg example">
-
+                        {
+                            listDistrict.map((district, index) => <option value={index+1}>{district}</option>)
+                        }
                     </select>
 
                     <div style={{clear: 'both', padding: '1%'}}></div>
---
+++ src/pages/register/hooks/Cities.js
@@ -0,0 +1,292 @@
+
+export default function Cities() {
+    return [
+        {
+            id: 1,
+            name:"Hà Nội",
+            dist: [
+                "Hoàn Kiếm", "Ba Đình", "Đống Đa", "Hai Bà Trưng", "Thanh Xuân", "Tây Hồ", "Cầu Giấy", "Hoàng Mai", "Long Biên", "Đông Anh", "Gia Lâm", "Sóc Sơn", "Thanh Trì", "Nam Từ Liêm", "Hà Đông", "Sơn Tây", "Mê Linh", "Ba Vì", "Phúc Thọ", "Đan Phượng", "Hoài Đức", "Quốc Oai", "Thạch Thất", "Chương Mỹ", "Thanh Oai", "Thường Tín", "Phú Xuyên", "Ứng Hòa", "Mỹ Đức", "Bắc Từ Liêm"
+            ]
+        }, 
+        {
+            id: 2,
+            name: "TP Hồ Chí Minh",
+            dist: [
+                "Quận 1", "Quận 2", "Quận 3", "Quận 4", "Quận 5", "Quận 6", "Quận 7", "Quận 8", "Quận 9", "Quận 10", "Quận 11", "Quận 12", "Bình Tân", "Bình Thạnh", "Gò Vấp", "Phú Nhuận", "Tân Bình", "Tân Phú", "Thủ Đức", "Bình Chánh", "Cần Giờ", "Củ Chi", "Hóc Môn", "Nhà Bè"
+            ]
+        }, 
+        {
+            id: 3,
+            name: "Bình Dương",
+            dist: [
+                "Bến Cát", "Dầu Tiếng", "Dĩ An", "Phú Giáo", "Tân Uyên", "Thuận An", "Thủ Dầu Một", "Bàu Bàng"
+            ]
+        }, 
+        {
+            id: 4,
+            name: "Đà Nẵng",
+            dist: [
+                "Cẩm Lệ", "Hải Châu", "Liên Chiểu", "Ngũ Hành Sơn", "Sơn Trà", "Thanh Khê", "Hòa Vang", "Hoàng Sa"
+            ]
+        }, 
+        {
+            id: 5,
+            name: "Hải Phòng",
+            dist: [
+
+            ]
+        },
+        {
+            id: 6,
+            name: "Long An",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 7,
+            name: "Bà Rịa Vũng Tàu",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 8,
+            name: "An Giang",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 9,
+            name: "Bắc Giang",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 10,
+            name: "Bắc Kạn",
+            dist: [
+
+            ]
+        },
+        {
+            id: 11,
+            name: "Bạc Liêu",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 12,
+            name: "Bắc Ninh",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 13,
+            name: "Bến Tre",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 14,
+            name: "Bình Định",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 15,
+            name: "Bình Phước",
+            dist: [
+
+            ]
+        },
+        {
+            id: 16,
+            name: "Bình Thuận",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 17,
+            name: "Cà Mau",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 18,
+            name: "Cần Thơ",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 19,
+            name: "Cao Bằng",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 20,
+            name: "Đắk Lắk",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 21,
+            name: "Đắk Nông",
+            dist: [
+
+            ]
+        },
+        {
+            id: 22,
+            name: "Điện Biên",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 23,
+            name: "Đồng Nai",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 24,
+            name: "Đồng Tháp",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 25,
+            name: "Gia Lai",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 26,
+            name: "Hà Giang",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 27,
+            name: "Hà Nam",
+            dist: [
+
+            ]
+        },
+        {
+            id: 28,
+            name: "Hà Tĩnh",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 29,
+            name: "Hải Dương",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 30,
+            name: "Hậu Giang",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 31,
+            name: "Hoà Bình",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 32,
+            name: "Hưng Yên",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 33,
+            name: "Khánh Hoà",
+            dist: [
+
+            ]
+        },
+        {
+            id: 34,
+            name: "Kiên Giang",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 35,
+            name: "Kon Tum",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 36,
+            name: "Lai Châu",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 37,
+            name: "Lâm Đồng",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 38,
+            name: "Lạng Sơn",
+            dist: [
+
+            ]
+        }, 
+        {
+            id: 39,
+            name: "Lào Cai",
+            dist: [
+                
+            ]
+        },
+        {
+            id: 40,
+            name: "Nam Định",
+            dist: [
+                
+            ]
+        },
+        {
+            id: 41,
+            name: "Nghệ An",
+            dist: [
+                
+            ]
+        }
+    ];
+}
